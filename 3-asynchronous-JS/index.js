47eac3fcc04ccafaaca6c6917c3275b9477bbd84
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2025-01-13 09:24:49 +0530

    JS Promises

---
+++ 3-asynchronous-JS/index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const { resolve } = require('path');
+const superagent = require('superagent');
+const { reject } = require('superagent/lib/request-base');
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I coould not fund that file ');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write the file');
+      resolve('success');
+    });
+  });
+};
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    return writeFilePro('dog-image.txt', res.body.message);
+
+    // fs.writeFile('dog-image.txt', res.body.message, (err) => {
+    //   if (err) return console.log(err.message);
+    //   console.log('Random dog image saved to file!');
+    // });
+  })
+  .then(() => {
+    console.log('Random dog image saved to file!');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
